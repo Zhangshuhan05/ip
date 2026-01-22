ae99f62030d002ef8549299ad801f891fa68c6e3
zhangshuhan05 <zshuhan512@gmail.com>
2026-01-22 11:04:17 +0800

    added list ability and indentation to output, level 2

--- src/main/java/Charmie.java
+++ src/main/java/Charmie.java
@@ -1,24 +1,36 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Charmie {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Charmie");
-        System.out.println("What can I do for you? ;)");
-        System.out.println("____________________________________________________________");
+        String INDENT = "    ";
+        List<String> list = new ArrayList<>();
+
+        System.out.println(INDENT + "____________________________________________________________");
+        System.out.println(INDENT + "Hello! I'm Charmie");
+        System.out.println(INDENT + "What can I do for you? ;)");
+        System.out.println(INDENT + "____________________________________________________________");
         System.out.println();
         Scanner reader = new Scanner(System.in);
         while (true) {
             String input = reader.nextLine();
             if (input.equals("bye")) {
-                System.out.println("____________________________________________________________");
-                System.out.println("Byee. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
+                System.out.println(INDENT + "____________________________________________________________");
+                System.out.println(INDENT + "Byee. Hope to see you again soon!");
+                System.out.println(INDENT + "____________________________________________________________");
                 return;
+            } else if (input.equals("list")) {
+                System.out.println(INDENT + "____________________________________________________________");
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(INDENT + (i+1) + ". " + list.get(i));
+                }
+                System.out.println(INDENT + "____________________________________________________________");
             } else {
-                System.out.println("____________________________________________________________");
-                System.out.println(input);
-                System.out.println("____________________________________________________________");
+                list.add(input);
+                System.out.println(INDENT + "____________________________________________________________");
+                System.out.println(INDENT + "added: " + input);
+                System.out.println(INDENT + "____________________________________________________________");
                 System.out.println();
             }
         }
