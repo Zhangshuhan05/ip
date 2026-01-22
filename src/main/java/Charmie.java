2927bca345bd85143b8f2fe666e49122e63c5341
zhangshuhan05 <zshuhan512@gmail.com>
2026-01-22 11:56:50 +0800

    added new class Task and added marking ability, level-3

--- src/main/java/Charmie.java
+++ src/main/java/Charmie.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Charmie {
     public static void main(String[] args) {
         String INDENT = "    ";
-        List<String> list = new ArrayList<>();
+        List<Task> list = new ArrayList<>();
 
         System.out.println(INDENT + "____________________________________________________________");
         System.out.println(INDENT + "Hello! I'm Charmie");
@@ -15,19 +15,39 @@ public class Charmie {
         Scanner reader = new Scanner(System.in);
         while (true) {
             String input = reader.nextLine();
-            if (input.equals("bye")) {
+            Scanner inputScanner = new Scanner(input);
+            String instruction = inputScanner.next();
+            if (instruction.equals("bye")) {
                 System.out.println(INDENT + "____________________________________________________________");
                 System.out.println(INDENT + "Byee. Hope to see you again soon!");
                 System.out.println(INDENT + "____________________________________________________________");
                 return;
-            } else if (input.equals("list")) {
+            } else if (instruction.equals("list")) {
                 System.out.println(INDENT + "____________________________________________________________");
+                System.out.println(INDENT + "Here are the tasks in your list:");
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.println(INDENT + (i+1) + ". " + list.get(i));
+                    Task task = list.get(i);
+                    System.out.println(INDENT + (i+1) + ". [" + task.getStatusIcon() + "] " + task.description);
                 }
                 System.out.println(INDENT + "____________________________________________________________");
+                System.out.println();
+            } else if (instruction.equals("mark")) {
+                int index = inputScanner.nextInt() - 1;
+                if (index >= 0 && index < list.size()) {
+                    Task toMark = list.get(index);
+                    toMark.markAsDone();
+                    System.out.println(INDENT + "____________________________________________________________");
+                    System.out.println(INDENT + "Nicee! I've marked this task as done:");
+                    System.out.println(INDENT + "[" + toMark.getStatusIcon() + "]" + toMark.description);
+                    System.out.println(INDENT + "____________________________________________________________");
+                    System.out.println();
+                } else {
+                    System.out.println("invalid task number, pls try again");
+                    System.out.println();
+                }
             } else {
-                list.add(input);
+                Task newTask = new Task(input);
+                list.add(newTask);
                 System.out.println(INDENT + "____________________________________________________________");
                 System.out.println(INDENT + "added: " + input);
                 System.out.println(INDENT + "____________________________________________________________");
