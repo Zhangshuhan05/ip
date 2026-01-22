4f2f93bf41ec0c3e25fb57d30dd4a4f95312928c
zhangshuhan05 <zshuhan512@gmail.com>
2026-01-22 10:04:52 +0800

    Gave charmie echoing abilities, level 1

--- src/main/java/Charmie.java
+++ src/main/java/Charmie.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Charmie {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Charmie");
-        System.out.println("What can I do for you?");
+        System.out.println("What can I do for you? ;)");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println();
+        Scanner reader = new Scanner(System.in);
+        while (true) {
+            String input = reader.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Byee. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                return;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(input);
+                System.out.println("____________________________________________________________");
+                System.out.println();
+            }
+        }
+
     }
 }
