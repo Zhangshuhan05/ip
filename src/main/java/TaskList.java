f01ba72cfb21901ff859a2769ffe02652f579339
zhangshuhan05 <zshuhan512@gmail.com>
2026-01-29 17:59:36 +0800

    Add TaskList.java

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.*;
 
 public class TaskList {
+    private List<Task> list;
+
+    TaskList() {
+        this.list = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        this.list.add(task);
+    }
+
+    public Task markTask(int index) {
+        Task task = getTask(index);
+        task.markAsDone();
+        return task;
+    }
+
+    public Task unmarkTask(int index) {
+        Task task = getTask(index);
+        task.unMark();
+        return task;
+    }
+
+    public void removeTask(int index) {
+        this.list.remove(index);
+    }
+
+    public int getSize() {
+        return this.list.size();
+    }
+
+    public Task getTask(int index) {
+        return this.list.get(index);
+    }
 }
